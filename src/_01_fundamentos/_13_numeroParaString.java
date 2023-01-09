f376b842013003a683611007d2805720ed9b1550
fabiano alves <fasnaweb2020@gmail.com>
2023-01-09 13:41:52 -0300

    Como converter número para String

---
+++ src/_01_fundamentos/_13_numeroParaString.java
@@ -0,0 +1,14 @@
+package _01_fundamentos;
+
+public class _13_numeroParaString {
+    public static void main(String[] args) {
+      Integer num1 = 10000;
+        System.out.println(num1.toString().length());
+
+        int num2 = 100000;
+        System.out.println(Integer.toString(num2).length());
+
+        System.out.println(("" + num1).length());
+        System.out.println(("" + num2).length());
+    }
+}
