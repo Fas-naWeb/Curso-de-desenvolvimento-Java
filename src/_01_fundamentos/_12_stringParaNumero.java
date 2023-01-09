20d553b26f1acd6d4d2b5fa95a59573f9950acca
fabiano alves <fasnaweb2020@gmail.com>
2023-01-09 10:15:34 -0300

    Como converter String para Numero

--- src/_01_fundamentos/_12_stringParaNumero.java
+++ src/_01_fundamentos/_12_stringParaNumero.java
@@ -3,5 +3,6 @@ package _01_fundamentos;
 public class _12_stringParaNumero {
     public static void main(String[] args) {
         System.out.println("Organizando codigo novamente.");
+        System.out.println("Organizando codigo novamente.");
     }
 }
