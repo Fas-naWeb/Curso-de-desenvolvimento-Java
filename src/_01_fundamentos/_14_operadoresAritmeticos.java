1dd76b3382390370da8c13bd9247949a53bf0806
fabiano alves <fasnaweb2020@gmail.com>
2023-01-09 13:51:04 -0300

    Sobre operadores Aritmeticos

---
+++ src/_01_fundamentos/_14_operadoresAritmeticos.java
@@ -0,0 +1,24 @@
+package _01_fundamentos;
+
+import java.util.Scanner;;
+
+public class _14_operadoresAritmeticos {
+    public static void main(String[] args) {
+        Scanner entradas =  new Scanner(System.in);
+        double a = 0;
+        double b = 0;
+
+        System.out.println("Entre com um primeiro valor:");
+        a = entradas.nextDouble();
+
+        System.out.println("Entre com um segundo valor:");
+        b = entradas.nextDouble();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+    }
+}
