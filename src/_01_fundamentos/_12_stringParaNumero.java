703c9526686d93b2a27ade43588caa36076ca1f6
fabiano alves <fasnaweb2020@gmail.com>
2023-01-09 13:30:44 -0300

    Entendendo como converter String numerica em valor Númerico

--- src/_01_fundamentos/_12_stringParaNumero.java
+++ src/_01_fundamentos/_12_stringParaNumero.java
@@ -1,8 +1,21 @@
 package _01_fundamentos;
 
+import javax.swing.JOptionPane;
+
 public class _12_stringParaNumero {
     public static void main(String[] args) {
-        System.out.println("Organizando codigo novamente.");
-        System.out.println("Organizando codigo novamente.");
+        String valor1 = JOptionPane.showInputDialog("Digite o primeiro número:");
+        String valor2 = JOptionPane.showInputDialog("Digite o segundo número:");
+
+        System.out.println(valor1 + valor2);
+
+        double num1 = Double.parseDouble(valor1);
+        double num2 = Double.parseDouble(valor2);
+
+        double soma = num1 + num2;
+
+        System.out.println("Soma é: "+ soma);
+        System.out.println("Média é: "+ soma / 2);
+
     }
 }
