3a9225aa1fc7d72d343953756257ad102dad02dd
fabiano alves <fasnaweb2020@gmail.com>
2023-01-09 21:55:03 -0300

    Entendendo os operadores unarios

---
+++ src/_01_fundamentos/_17_OperadoresUnarios.java
@@ -0,0 +1,17 @@
+package _01_fundamentos;
+
+public class _17_OperadoresUnarios {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        a++; // a = a + 1
+        a--; // b = b - 1
+
+        b--;
+        b++;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
