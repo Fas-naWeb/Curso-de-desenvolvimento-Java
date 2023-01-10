4266b4171073a0f499c1c9f11e3258807a1d48c4
fabiano alves <fasnaweb2020@gmail.com>
2023-01-10 00:20:58 -0300

    Sobre a estrutura de controle if else if

---
+++ src/_02_EstruturasDeControle/_03_EstruturaIfElseIf.java
@@ -0,0 +1,26 @@
+package _02_EstruturasDeControle;
+
+import java.util.Scanner;
+
+public class _03_EstruturaIfElseIf {
+    public static void main(String[] args) {
+        
+        Scanner entradas = new Scanner(System.in);
+        
+        System.out.println("Digite uma nota:");
+        double nota1 = entradas.nextDouble();
+
+        if (nota1 > 10 || nota1 < 0) {
+             System.out.println("Nota é invalida!");
+        }else if(nota1 >= 8.1){
+            System.out.println("Conceito A");
+        }else if(nota1 >= 4.1 && nota1 < 8.1){
+            System.out.println("Conceito B");
+        }else{
+            System.out.println("Conceito C");
+        }
+
+        System.out.println("Fim.....");
+        entradas.close();
+    }
+}
