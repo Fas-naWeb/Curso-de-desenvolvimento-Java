1ec2eb333a0d32b37cf4f1589cd8d5e7eda8fe68
fabiano alves <fasnaweb2020@gmail.com>
2023-01-10 16:03:47 -0300

    Entendendo a estrutura Continue

---
+++ src/_02_EstruturasDeControle/_09_Continue.java
@@ -0,0 +1,14 @@
+package _02_EstruturasDeControle;
+
+public class _09_Continue {
+    public static void main(String[] args) {
+        int n = 0;
+        for(int i = 0; i <= 50;i++){
+            System.out.println(i);
+            if(i == 10){
+                System.out.println("Pulou o numero 10");
+                continue;
+            }
+        }
+    }
+}
