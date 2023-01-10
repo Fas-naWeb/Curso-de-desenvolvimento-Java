6dbe3ca8bab65de5a93113ecb2b815a206250203
fabiano alves <fasnaweb2020@gmail.com>
2023-01-09 23:44:23 -0300

    Comecando as estruturas de controle

---
+++ src/_02_EstruturasDeControle/_01_EstruturaDeControleIf.java
@@ -0,0 +1,17 @@
+package _02_EstruturasDeControle;
+
+public class _01_EstruturaDeControleIf {
+    public static void main(String[] args) {
+        /*
+         * E uma estrutura de decisão simples apenas verifica se a condição e verdsdeira ou falsa
+         * caso seja verdadeira entra e executa o código se não,não faz nada continua na proxima sequencia 
+         * do codigo.
+         */
+
+         double nota = 12;
+
+         if(nota >= 5){
+            System.out.println("Você entrou no quadro de Honra!!!");
+         }
+    }
+}
---
+++ src/_02_EstruturasDeControle/_02_EstruturaIfElse.java
@@ -0,0 +1,7 @@
+package _02_EstruturasDeControle;
+
+public class _02_EstruturaIfElse {
+    public static void main(String[] args) {
+        
+    }
+}
