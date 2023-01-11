27a307523d17a4ed12b3047068b7c29cea5738c3
fabiano alves <fasnaweb2020@gmail.com>
2023-01-11 00:12:06 -0300

    Conceitos de metodos

--- src/_03_classesEMetodos/_02_ConceitoDeMetodos.java
+++ src/_03_classesEMetodos/_02_ConceitoDeMetodos.java
@@ -5,7 +5,7 @@ public class _02_ConceitoDeMetodos {
        Metodos são codigos que executa alguma ação,e o verbo.Eles podem ou não receber parametros e podem ou não 
        retornar algum valor.Você pode ter metodos com o mesmo nome,mais não com a mesma assinatura.
      */
-     // Metodo recebe dois parametros e retorna a soma
+     // Metodo recebe dois parametros e retorna a soma.
      int soma(int a,int b){
        return a + b;
      }
