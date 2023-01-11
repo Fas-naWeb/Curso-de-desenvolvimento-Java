4c55a00b7d31f7849ee2da43478a278a1722d9fc
fabiano alves <fasnaweb2020@gmail.com>
2023-01-11 01:16:25 -0300

    Entendendo o Continue

--- src/_02_EstruturasDeControle/_09_Continue.java
+++ src/_02_EstruturasDeControle/_09_Continue.java
@@ -2,7 +2,6 @@ package _02_EstruturasDeControle;
 
 public class _09_Continue {
     public static void main(String[] args) {
-        int n = 0;
         for(int i = 0; i <= 50;i++){
             System.out.println(i);
             if(i == 10){
