d1f52ccd36717e09870f50596e2d450a7329d20e
fabiano alves <fasnaweb2020@gmail.com>
2023-01-11 13:37:29 -0300

    Trabalhando com valor padrao de variavei

---
+++ src/_03_classesEMetodos/_08_ValorPadraoVariavel.java
@@ -0,0 +1,29 @@
+package _03_classesEMetodos;
+
+public class _08_ValorPadraoVariavel {
+    String nome;
+
+    _08_ValorPadraoVariavel(String nome){
+        this.nome = nome;
+    }
+
+    public static void main(String[] args) {
+        /*
+           Variaveis criadas dentro de classe como atributo iniciali-se com valores padrao,no caso
+           quando voce programador nao define nada.
+           
+           bute,short,int,long -> O valor padrao e 0
+           float,double -> Ovalor padrao e 0.0
+           boolean -> O valor padrao e false
+           char -> '\u0000'
+           
+           Mais quando voce define uma variavel local a mesma nao possui valor padrao voce tem que definir 
+           o valor se nao ocorre um erro.
+        */
+
+        int numero;
+        int numero2 = 19;
+        System.out.println(numero); // Note que aqui gera um erro pois a variavel local nao foi inicializada.
+        System.out.println(numero2); // Note que aqui nao gera um erro pois a variavel local  foi inicializada.
+    }
+}
