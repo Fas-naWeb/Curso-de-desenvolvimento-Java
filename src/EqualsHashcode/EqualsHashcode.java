63d86db29c18ef1797f71f60b56ec40242c0860e
fabiano alves <fasnaweb2020@gmail.com>
2023-01-17 17:35:46 -0300

    Equals e Hashcode trabalham juntos

--- src/EqualsHashcode/EqualsHashcode.java
+++ src/EqualsHashcode/EqualsHashcode.java
@@ -14,7 +14,7 @@ public class EqualsHashcode {
 		System.out.println(u1.equals(u2));
 		System.out.println(u2.equals(u1));
 		
-		System.out.println(u1 == u1);
+		//System.out.println(u1 == u1);
 		System.out.println(u2.equals(u2));
 		
 	}
