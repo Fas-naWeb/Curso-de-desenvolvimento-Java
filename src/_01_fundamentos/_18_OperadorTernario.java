00c4f1f50796ecc2a4d4f518981e8b4eb877e2cc
fabiano alves <fasnaweb2020@gmail.com>
2023-01-17 15:14:03 -0300

    Operador ternario

--- src/_01_fundamentos/_18_OperadorTernario.java
+++ src/_01_fundamentos/_18_OperadorTernario.java
@@ -4,7 +4,7 @@ public class _18_OperadorTernario {
 
 	public static void main(String[] args) {
 		double media = 7.6;
-        String resultado = (media >= 7.0)? "Aprovado!" : "Resultado :(";
+        String resultado = (media >= 7.0)? "Aprovado!!" : "Resultado :(";
 
         System.out.println("O aluno está " + resultado );
 	}
