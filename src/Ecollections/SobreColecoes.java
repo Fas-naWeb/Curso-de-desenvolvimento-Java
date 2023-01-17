c506b489b633d4aa0a8a780a699ab2ba355181a6
fabiano alves <fasnaweb2020@gmail.com>
2023-01-17 18:06:29 -0300

    Tipos de coleções em java

---
+++ src/Ecollections/SobreColecoes.java
@@ -0,0 +1,21 @@
+package Ecollections;
+
+public class SobreColecoes {
+
+	/*
+	 * 1° Coleção Set - Caracteristicas Não ordenado Não indexado Não aceita
+	 * repetição
+	 * 
+	 * 
+	 * 2° Coleção List - Caracteristicas É indexado Aceita repetição
+	 * 
+	 * 3° Coleção Map - Caracteristicas É uma estrutura de chave/valor Chave Não
+	 * aceita repetição Valor aceita repetição
+	 * 
+	 * 4° Coleção Queue -(Fila)-Caracteristicas Implementa uma fila First in/first
+	 * out ou (Fifo)
+	 * 
+	 * 5° Coleção Stack (Pilha)- Caracteristicas Implementa pilha (Lifo) Last in /
+	 * First Out (Lifo)
+	 */
+}
