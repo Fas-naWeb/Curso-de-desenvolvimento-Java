77cdbc32ea3ee2bc96aad9f679bcef80785248c8
fabiano alves <fasnaweb2020@gmail.com>
2023-01-17 15:17:11 -0300

    Estrutura de controle if

--- src/_02_EstruturasDeControle/_01_EstruturaDeControleIf.java
+++ src/_02_EstruturasDeControle/_01_EstruturaDeControleIf.java
@@ -3,6 +3,7 @@ package _02_EstruturasDeControle;
 public class _01_EstruturaDeControleIf {
     public static void main(String[] args) {
         /*
+         * 
          * E uma estrutura de decisão simples apenas verifica se a condição e verdsdeira ou falsa
          * caso seja verdadeira entra e executa o código se não,não faz nada continua na proxima sequencia 
          * do codigo.
