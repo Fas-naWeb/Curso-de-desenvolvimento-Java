e547d722b92b5abcc45304f1259b33bbd24ec95e
fabiano alves <fasnaweb2020@gmail.com>
2023-01-17 15:27:58 -0300

    Conceito de classe

--- src/_03_classesEMetodos/_01_ConceitoDeClasse.java
+++ src/_03_classesEMetodos/_01_ConceitoDeClasse.java
@@ -4,6 +4,6 @@ public class _01_ConceitoDeClasse {
   /*
      Classe é uma forma,um molde que você cria para gerar um tipo de dado.É como uma planta baixa de uma casa 
      onde através dela você pode criar varias casas do mesmo tipo,mais com caracteristicas diferente.
-     As classes são compostas de variaveis, constantes ,metodos construtores e metodos..
+     As classes são compostas de variaveis, constantes ,metodos construtores e metodos...
    */
 }
