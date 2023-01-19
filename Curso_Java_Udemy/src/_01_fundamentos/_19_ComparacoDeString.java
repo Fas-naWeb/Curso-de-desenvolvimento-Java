72dcd792b3f23dd9cfdc05838873d2a06786a7b8
fabiano alves <fasnaweb2020@gmail.com>
2023-01-19 13:46:20 -0300

    Comparação de Strings

--- Curso_Java_Udemy/src/_01_fundamentos/_19_ComparacoDeString.java
+++ Curso_Java_Udemy/src/_01_fundamentos/_19_ComparacoDeString.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class _19_ComparacoDeString {
     public static void main(String[] args) {
        System.out.println("2" == "2"); 
-
+       //
        String s1 = new String("2");
        System.out.println("2" == s1);
        System.out.println("2".equals(s1));
