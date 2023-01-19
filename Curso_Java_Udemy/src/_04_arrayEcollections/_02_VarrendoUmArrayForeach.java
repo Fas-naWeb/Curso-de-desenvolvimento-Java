23598e02f82b1647a036551b4c363077861dd027
fabiano alves <fasnaweb2020@gmail.com>
2023-01-19 14:03:02 -0300

    Sobre Foreach

--- Curso_Java_Udemy/src/_04_arrayEcollections/_02_VarrendoUmArrayForeach.java
+++ Curso_Java_Udemy/src/_04_arrayEcollections/_02_VarrendoUmArrayForeach.java
@@ -1,7 +1,7 @@
 public class _02_VarrendoUmArrayForeach {
     public static void main(String[] args) {
         double[] notasAlunos = {9.2,1.3,4.5,6.7,8.9};
-        // foreach em java nao se escreve foreach usa-se o for mas com nomeclatura diferente
+        // foreach em java nao se escreve foreach usa-se o for mas com nomeclatura diferente.
         for(double nota: notasAlunos){
             System.out.println(nota);
         }
