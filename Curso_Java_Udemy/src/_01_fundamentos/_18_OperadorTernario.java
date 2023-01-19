b05ee88d2c95ff3db24ef12456e1fb4ea4ec87af
fabiano alves <fasnaweb2020@gmail.com>
2023-01-19 13:45:43 -0300

    Operador Ternário

--- Curso_Java_Udemy/src/_01_fundamentos/_18_OperadorTernario.java
+++ Curso_Java_Udemy/src/_01_fundamentos/_18_OperadorTernario.java
@@ -3,7 +3,7 @@ package _01_fundamentos;
 public class _18_OperadorTernario {
     public static void main(String[] args) {
         double media = 7.6;
-        String resultado = (media >= 7.0)? "Aprovado!" : "Resultado :(";
+        String resultado = (media >= 7.0)? "Aprovado!" : "Resultado :( ";
 
         System.out.println("O aluno está " + resultado );
     }
